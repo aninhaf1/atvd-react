2110c8190a88b488c13285a06b77faf90ae589b7
Ana Carolina <84184662+aninhaf1@users.noreply.github.com>
2023-05-30 11:55:02 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './style.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  
+  return (
+    <>
+    <body>
+      <div class='tela-inteira'>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </div>
+    </body>
+    </>
+    
+  );
+}
+export default App;
